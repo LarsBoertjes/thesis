ec77c2c7c8721866b006da57edc1060fe7af6cdc
Lars Boertjes <l.c.boertjes@student.tudelft.nl>
2025-02-12 14:07:53 +0100

    PointProjection3Dto2D
    Experimenting with the Geodelta.Photogrammetry and Geodelta.Units API

--- code/getImageCoordinatesInOblique/Program.cs
+++ code/getImageCoordinatesInOblique/Program.cs
@@ -1,2 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using BAG;
+using NetTopologySuite.Geometries; // For handling geometric data
+using System;
+using System.Collections.Generic;
+using Geodelta.Photogrammetry;
+using Geodelta.Units;
+using Geodelta.Units.Length;
+using System.Security.Cryptography.X509Certificates;
+
+namespace TerrainPointExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            Millimeter millimeter = new Millimeter();
+
+            TerrainPoint<Millimeter> point = new TerrainPoint<Millimeter>(
+                    new Quantity<Millimeter>(100.0, millimeter),
+                    new Quantity<Millimeter>(200.0, millimeter),
+                    new Quantity<Millimeter>(50.0, millimeter),
+                    "SurveyPoint1");
+
+            Console.WriteLine(point.ToString());
+
+            Meter meter = new Meter();
+
+            TerrainPoint<Meter> convertedPoint = point.As(meter);
+            Console.WriteLine(convertedPoint.ToString());
+        }
+    }
+}
